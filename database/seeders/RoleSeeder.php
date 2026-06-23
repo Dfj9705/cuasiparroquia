d4135908bed1f182b06b43473bb7ff09708382b9
Abner Daniel Fuentes Juárez <abnerfuentes05@gmail.com>
2026-06-23 16:51:32 -0600

    agrega seeder de user

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
@@ -16,5 +18,13 @@ class RoleSeeder extends Seeder
         Role::firstOrCreate(['name' => 'Super Admin']);
         Role::firstOrCreate(['name' => 'Administrador']);
         Role::firstOrCreate(['name' => 'Editor']);
+
+        $user = User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@csma.com',
+            'password' => Hash::make('Asdf123#'),
+        ]);
+
+        $user->assignRole('Super Admin');
     }
 }
